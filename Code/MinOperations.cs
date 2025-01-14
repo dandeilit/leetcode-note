0edfda5b29ff2b80b1048e65c779fbd0ecec4bec
dandeilit <dande.i.lit@gmail.com>
2025-01-14 14:08:20 +0800

    add 3065. Minimum Operations to Exceed Threshold Value I

---
+++ Code/MinOperations.cs
@@ -0,0 +1,31 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 3065. Minimum Operations to Exceed Threshold Value I
+    /// 
+    /// You are given a 0-indexed integer array nums, and an integer k.
+    /// 给定一个0 索引的整数数组nums和一个整数k 。
+    /// 
+    /// In one operation, you can remove one occurrence of the smallest element of nums.
+    /// 在一次操作中，您可以删除nums中最小元素的一次出现。
+    /// 
+    /// Return the minimum number of operations needed so that all elements of the array are greater than or equal to k.
+    /// 返回使数组的所有元素都大于或等于k所需的最少操作数。
+    /// 
+    /// </summary>
+    public class MinOperations
+    {
+        public int Solution(int[] nums, int k)
+        {
+            int ans = 0;
+            foreach (var num in nums)
+            {
+                if (num < k)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ Test/MinOperationsTest.cs
@@ -0,0 +1,24 @@
+﻿using Code;
+
+namespace Test
+{
+    public class MinOperationsTest
+    {
+        public static TheoryData<int[], int, int> TestData => new TheoryData<int[], int, int>()
+        {
+            { [2,11,10,1,3], 10, 3 },
+            { [1,1,2,4,9], 1, 0},
+            { [1,1,2,4,9], 9, 4},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, int k, int expected)
+        {
+            var minOperations = new MinOperations();
+            var actual = minOperations.Solution(nums, k);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
