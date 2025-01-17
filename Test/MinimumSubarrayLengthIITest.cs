6c1d8bc7f1e3b808c0e59da9b9ac2c9a68dbce6f
dandeilit <dande.i.lit@gmail.com>
2025-01-17 14:15:35 +0800

    add 3097. Shortest Subarray With OR at Least K II

---
+++ Test/MinimumSubarrayLengthIITest.cs
@@ -0,0 +1,24 @@
+﻿using Code;
+
+namespace Test
+{
+    public class MinimumSubarrayLengthIITest
+    {
+        public static TheoryData<int[], int, int> TestData => new TheoryData<int[], int, int>()
+        {
+            { [1,2,3], 2, 1},
+            { [2,1,8], 10, 3},
+            { [1,2], 0, 1},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, int k, int expected)
+        {
+            var minimumSubarrayLengthII = new MinimumSubarrayLengthII();
+            var actual = minimumSubarrayLengthII.Solution(nums, k);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
