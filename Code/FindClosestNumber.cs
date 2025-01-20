43c492bd04277c47bbb42e0fbe69debb9c5c2b2a
dandeilit <dande.i.lit@gmail.com>
2025-01-20 13:47:48 +0800

    add 2239. Find Closest Number to Zero

---
+++ Code/FindClosestNumber.cs
@@ -0,0 +1,29 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 2239. Find Closest Number to Zero
+    /// 
+    /// Given an integer array nums of size n, return the number with the value closest to 0 in nums. If there are multiple answers, return the number with the largest value.
+    /// 给定一个大小为 n 的整数数组 nums，返回 nums 中值最接近 0 的数字。如果有多个答案，则返回值最大的数字。
+    /// 
+    /// </summary>
+    public class FindClosestNumber
+    {
+        public int Solution(int[] nums)
+        {
+            int ans = int.MaxValue;
+            foreach (var num in nums)
+            {
+                int absN = Math.Abs(num);
+                int absA = Math.Abs(ans);
+                if (absN > absA || (absN == absA && num <= ans))
+                {
+                    continue;
+                }
+
+                ans = num;
+            }
+            return ans;
+        }
+    }
+}
---
+++ Test/FindClosestNumberTest.cs
@@ -0,0 +1,23 @@
+﻿using Code;
+
+namespace Test
+{
+    public class FindClosestNumberTest
+    {
+        public static TheoryData<int[], int> TestData => new TheoryData<int[], int>()
+        {
+            { [-4,-2,1,4,8], 1},
+            { [2,-1,1], 1},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, int expected)
+        {
+            var findClosestNumber = new FindClosestNumber();
+            var actual = findClosestNumber.Solution(nums);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
