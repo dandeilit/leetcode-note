88f67cf878dc66a1b139170406b3fb1d3c7d1cbf
dandeilit <dande.i.lit@gmail.com>
2025-01-24 13:57:36 +0800

    add 2944. Minimum Number of Coins for Fruits

---
+++ Code/MinimumCoins.cs
@@ -0,0 +1,88 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 2944. Minimum Number of Coins for Fruits
+    /// 
+    /// You are given an 1-indexed integer array prices where prices[i] denotes the number of coins needed to purchase the ith fruit.
+    /// 定一个 1 索引整数数组 prices，其中 prices[i] 表示购买第 i 个水果所需的硬币数量。
+    /// 
+    /// The fruit market has the following reward for each fruit:
+    /// 水果市场对每种水果的奖励如下：
+    /// 
+    /// * If you purchase the ith fruit at prices[i] coins, you can get any number of the next i fruits for free.
+    /// * 如果您以 prices[i] 硬币购买第 i 个水果，则可以免费获得接下来的 i 个水果中的任意数量。
+    /// 
+    /// Note: that even if you can take fruit j for free, you can still purchase it for prices[j] coins to receive its reward.
+    /// 注意：即使您可以免费获得水果 j，您仍然可以花费 prices[j] 硬币购买它以获得奖励。
+    /// 
+    /// Return the minimum number of coins needed to acquire all the fruits.
+    /// 返回获取所有水果所需的最少硬币数量。
+    /// 
+    /// </summary>
+    public class MinimumCoins
+    {
+        IDictionary<int, int> memo = new Dictionary<int, int>();
+        int[] prices;
+
+        /// <summary>
+        /// 记忆化搜索
+        /// </summary>
+        /// <param name="prices"></param>
+        /// <returns></returns>
+        public int Solution(int[] prices)
+        {
+            this.prices = prices;
+            return DP(0);
+        }
+
+        /// <summary>
+        /// 从下标 index 开始的所有水果需要花费的最少金币
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public int DP(int index)
+        {
+            if (2 * index + 2 >= prices.Length)
+            {
+                return prices[index];
+            }
+            if (!memo.ContainsKey(index))
+            {
+                int minValue = int.MaxValue;
+                for (int i = index + 1; i <= 2 * index + 2; i++)
+                {
+                    minValue = Math.Min(minValue, DP(i));
+                }
+                memo.Add(index, prices[index] + minValue);
+            }
+            return memo[index];
+        }
+
+        /// <summary>
+        /// 单调队列优化时间复杂度
+        /// 每一个 DP[i] 的值都依赖于后 i 个元素的结果，所以可以反向遍历避免递归
+        /// </summary>
+        /// <param name="prices"></param>
+        /// <returns></returns>
+        public int Solution2(int[] prices)
+        {
+            int n = prices.Length;
+            var queue = new LinkedList<int[]>();
+            queue.AddFirst(new int[] { n, 0 });
+            for (int i = n - 1; i >= 0; i--)
+            {
+                while (queue.Last.Value[0] >= 2 * i + 3)
+                {
+                    queue.RemoveLast();
+                }
+                int cur = queue.Last.Value[1] + prices[i];
+                while (queue.First.Value[1] >= cur)
+                {
+                    queue.RemoveFirst();
+                }
+                queue.AddFirst(new int[] { i, cur });
+            }
+            return queue.First.Value[1];
+        }
+    }
+}
---
+++ Test/MinimumCoinsTest.cs
@@ -0,0 +1,36 @@
+﻿using Code;
+
+namespace Test
+{
+    public class MinimumCoinsTest
+    {
+        public static TheoryData<int[], int> TestData => new TheoryData<int[], int>()
+        {
+            { [3,1,2],4},
+            { [1,10,1,1],2},
+            { [26,18,6,12,49,7,45,45],39},
+            { [14,37,37,38,24,15,12],63},
+            { [38,23,27,32,47,45,48,24,39,26,37,42,24,45,27,26,15,16,26,6],132},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] prices, int expected)
+        {
+            var minimumCoins = new MinimumCoins();
+            var actual = minimumCoins.Solution(prices);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test2(int[] prices, int expected)
+        {
+            var minimumCoins = new MinimumCoins();
+            var actual = minimumCoins.Solution2(prices);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
