f76792be36026dc21cf2845e66c408f8a0ef0423
dandeilit <dande.i.lit@gmail.com>
2025-01-27 12:21:55 +0800

    add 45. Jump Game II

---
+++ Code/Jump.cs
@@ -0,0 +1,44 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 45. Jump Game II
+    /// 
+    /// You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+    /// 给定一个长度为 n 的整数数组 nums，其索引为 0。您的初始位置为 nums[0]。
+    /// 
+    /// Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+    /// 每个元素 nums[i] 表示从索引 i 向前跳跃的最大长度。换句话说，如果您位于 nums[i]，则可以跳转到任何 nums[i + j]，其中：
+    /// 
+    /// * 0 <= j <= nums[i]
+    /// * i + j < n
+    /// 
+    /// Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+    /// 返回到达 nums[n - 1] 所需的最少跳跃次数。测试用例的生成方式使您可以到达 nums[n - 1]。
+    /// 
+    /// </summary>
+    public class Jump
+    {
+        /// <summary>
+        /// 贪心算法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int Solution(int[] nums)
+        {
+            int maxPos = 0, end = 0, steps = 0;
+            for (int i = 0; i < nums.Length - 1; ++i)
+            {
+                if (maxPos >= i)
+                {
+                    maxPos = Math.Max(maxPos, i + nums[i]);
+                    if (i == end)
+                    {
+                        end = maxPos;
+                        ++steps;
+                    }
+                }
+            }
+            return steps;
+        }
+    }
+}
---
+++ Test/JumpTest.cs
@@ -0,0 +1,27 @@
+﻿using Code;
+
+namespace Test
+{
+    public class JumpTest
+    {
+        public static TheoryData<int[], int> TestData => new TheoryData<int[], int>()
+        {
+            { [2,3,1,1,4],2},
+            { [2,3,0,1,4],2},
+            { [0],0},
+            { [1,2,3],2},
+            { [1,2],1},
+            { [9,7,9,1,7,3,7,0,5,0,0,5,6,7,5,6,0,5,4,7,3,9,0,2,0,5,9,2,3,6,0,4,3,1,6,3,4,3,3,1,3,3,8,1,2,3,0,3,1,4,2,6,5,4,0,6,9,0,8,4,6,0,9,2,1,7,5],12}
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, int expected)
+        {
+            var jump = new Jump();
+            var actual = jump.Solution(nums);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
