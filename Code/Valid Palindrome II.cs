3d8d14e7cf3fef15507a7c2e91ab3b91c46d061a
dandeilit <dande.i.lit@gmail.com>
2025-02-03 21:36:07 +0800

    add 680. Valid Palindrome II

---
+++ Code/Valid Palindrome II.cs
@@ -0,0 +1,49 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 680. Valid Palindrome II
+    /// 
+    /// Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+    /// 给定一个字符串 s，如果从中最多删除一个字符后 s 可以成为回文，则返回 true。
+    /// 
+    /// </summary>
+    public class Valid_Palindrome_II
+    {
+        /// <summary>
+        /// 贪心
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool ValidPalindrome(string s)
+        {
+            int l = 0, r = s.Length - 1;
+            while (l < r)
+            {
+                if (s[l] == s[r])
+                {
+                    l++;
+                    r--;
+                }
+                else
+                {
+                    return IsPalindrome(s, l + 1, r) || IsPalindrome(s, l, r - 1);
+                }
+            }
+            return true;
+        }
+
+        private bool IsPalindrome(string s, int l, int r)
+        {
+            while (l < r)
+            {
+                if (s[l] != s[r])
+                {
+                    return false;
+                }
+                l++;
+                r--;
+            }
+            return true;
+        }
+    }
+}
---
+++ Test/Valid Palindrome II Test.cs
@@ -0,0 +1,24 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Valid_Palindrome_II_Test
+    {
+        public static TheoryData<string, bool> TestData => new TheoryData<string, bool>()
+        {
+            { "aba",true},
+            { "abca",true},
+            { "abc",false},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(string s, bool expected)
+        {
+            var valid_Palindrome_II = new Valid_Palindrome_II();
+            var actual = valid_Palindrome_II.ValidPalindrome(s);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
