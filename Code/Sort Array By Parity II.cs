ce0abb36bd028f930dd1c628c4b1a16c8d8583d5
dandeilit <dande.i.lit@gmail.com>
2025-02-04 19:22:16 +0800

    add 922. Sort Array By Parity II

---
+++ Code/Sort Array By Parity II.cs
@@ -0,0 +1,52 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 922. Sort Array By Parity II
+    /// 
+    /// Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+    /// 给定一个整数数组 nums，nums 中一半的整数是奇数，另一半是偶数。
+    /// 
+    /// Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+    /// 对数组进行排序，使得每当 nums[i] 为奇数时，i 也为奇数，每当 nums[i] 为偶数时，i 也为偶数。
+    /// 
+    /// Return any answer array that satisfies this condition.
+    /// 返回满足此条件的任何答案数组。
+    /// 
+    /// Follow Up: Could you solve it in-place?
+    /// 进阶：你能就地解决它吗？
+    /// 
+    /// </summary>
+    public class Sort_Array_By_Parity_II
+    {
+        /// <summary>
+        /// 双指针
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] SortArrayByParityII(int[] nums)
+        {
+            int i = 0, j = 1;
+            int n = nums.Length;
+            while (i < n)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    i += 2;
+                }
+                else if (nums[j] % 2 == 1)
+                {
+                    j += 2;
+                }
+                else
+                {
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                    i += 2;
+                    j += 2;
+                }
+            }
+            return nums;
+        }
+    }
+}
---
+++ Test/Sort Array By Parity II Test.cs
@@ -0,0 +1,23 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Sort_Array_By_Parity_II_Test
+    {
+        public static TheoryData<int[], int[][]> TestData => new TheoryData<int[], int[][]>()
+        {
+            { [4,2,5,7],[[4,5,2,7],[4,7,2,5], [2,5,4,7], [2,7,4,5]]},
+            { [2,3],[[2,3]]},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, int[][] expected)
+        {
+            var sort_Array_By_Parity_II = new Sort_Array_By_Parity_II();
+            var actual = sort_Array_By_Parity_II.SortArrayByParityII(nums);
+
+            Assert.Contains(actual, expected);
+        }
+    }
+}
