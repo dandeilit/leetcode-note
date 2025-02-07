0a010e1d1a004dcf7e404e8873abe274a2df59e1
dandeilit <dande.i.lit@gmail.com>
2025-02-07 12:00:25 +0800

    add 59. Spiral Matrix II

---
+++ Code/Spiral Matrix II.cs
@@ -0,0 +1,71 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 59. Spiral Matrix II
+    /// 
+    /// Given a positive integer n, generate an n x n matrix filled with elements from 1 to Math.Pow(n, 2) in spiral order.
+    /// 给定一个正整数 n，生成一个 n x n 矩阵，其中以螺旋顺序填充元素从 1 到 Math.Pow(n, 2)。
+    /// 
+    /// 1  2  3  4
+    /// 12 13 14 5
+    /// 11 16 15 6
+    /// 10 9  8  7
+    /// 
+    /// 1  2  3  4  5
+    /// 16 17 18 19 6
+    /// 15 24 25 20 7
+    /// 14 23 22 21 8
+    /// 13 12 11 10 9
+    /// </summary>
+    public class Spiral_Matrix_II
+    {
+        /// <summary>
+        /// 模拟
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int[][] GenerateMatrix(int n)
+        {
+            var ans = new int[n][];
+            for (var i = 0; i < ans.Length; i++)
+            {
+                ans[i] = new int[n];
+            }
+
+            int x = 0, y = 0;
+            int max = (int)Math.Pow(n, 2);
+            bool flag = false;
+            int node = n;
+            int path = 0;
+            for (var i = 1; i <= max; i++)
+            {
+                ans[x][y] = i;
+
+                if (i == node)
+                {
+                    node += flag ? n : --n;
+                    flag = !flag;
+                    path++;
+                }
+
+                switch (path % 4)
+                {
+                    case 0:
+                        y++;
+                        break;
+                    case 1:
+                        x++;
+                        break;
+                    case 2:
+                        y--;
+                        break;
+                    case 3:
+                        x--;
+                        break;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ Test/Spiral Matrix II Test.cs
@@ -0,0 +1,23 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Spiral_Matrix_II_Test
+    {
+        public static TheoryData<int, int[][]> TestData => new TheoryData<int, int[][]>()
+        {
+            { 3,[[1,2,3],[8,9,4],[7,6,5]]},
+            { 1,[[1]]},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int n, int[][] expected)
+        {
+            var spiral_Matrix_II = new Spiral_Matrix_II();
+            var actual = spiral_Matrix_II.GenerateMatrix(n);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
