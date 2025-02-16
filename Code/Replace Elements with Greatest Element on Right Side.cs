3da37d50c71877e37e47bcf0a861293e799ba5a8
dandeilit <dande.i.lit@gmail.com>
2025-02-16 10:52:04 +0800

    add 1299. Replace Elements with Greatest Element on Right Side

---
+++ Code/Replace Elements with Greatest Element on Right Side.cs
@@ -0,0 +1,82 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 1299. Replace Elements with Greatest Element on Right Side
+    /// 
+    /// Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+    /// 给定一个数组 arr，将数组中的每个元素替换为右元素之间的最大元素，然后用-1替换最后一个元素。
+    /// 
+    /// After doing so, return the array.
+    /// 这样做后，返回数组。
+    /// 
+    /// </summary>
+    public class Replace_Elements_with_Greatest_Element_on_Right_Side
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            var n = arr.Length;
+            if (n == 1)
+            {
+                return [-1];
+            }
+
+            if (n == 2)
+            {
+                return [arr[1], -1];
+            }
+
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b - a));
+            for (var i = 1; i < n; i++)
+            {
+                queue.Enqueue(i, arr[i]);
+            }
+
+            int start = 0;
+            while (queue.Count > 0)
+            {
+                var inx = queue.Dequeue();
+                while (inx < start)
+                {
+                    inx = queue.Dequeue();
+                }
+
+                for (var i = start; i < inx; i++)
+                {
+                    arr[i] = arr[inx];
+                }
+                start = inx;
+
+                if (start == n - 1)
+                {
+                    arr[start] = -1;
+                    break;
+                }
+            }
+
+            return arr;
+        }
+
+        /// <summary>
+        /// 逆序遍历
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public int[] ReplaceElements2(int[] arr)
+        {
+            var n = arr.Length;
+            int max = 0;
+            int preNum = arr[n - 1];
+            for (var i = n - 1; i > 0; i--)
+            {
+                if (preNum > max)
+                {
+                    max = preNum;
+                }
+                preNum = arr[i - 1];
+                arr[i - 1] = max;
+            }
+            arr[n - 1] = -1;
+            return arr;
+        }
+    }
+}
---
+++ Test/Replace Elements with Greatest Element on Right Side Test.cs
@@ -0,0 +1,33 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Replace_Elements_with_Greatest_Element_on_Right_Side_Test
+    {
+        public static TheoryData<int[], int[]> TestData => new TheoryData<int[], int[]>()
+        {
+            { [17,18,5,4,6,1],[18,6,6,6,1,-1]},
+            { [400],[-1]},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] arr, int[] expected)
+        {
+            var replace_Elements_with_Greatest_Element_on_Right_Side = new Replace_Elements_with_Greatest_Element_on_Right_Side();
+            var actual = replace_Elements_with_Greatest_Element_on_Right_Side.ReplaceElements(arr);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test2(int[] arr, int[] expected)
+        {
+            var replace_Elements_with_Greatest_Element_on_Right_Side = new Replace_Elements_with_Greatest_Element_on_Right_Side();
+            var actual = replace_Elements_with_Greatest_Element_on_Right_Side.ReplaceElements2(arr);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
