1272af7f356198ad98a7c5e80a78893810ac8a10
dandeilit <dande.i.lit@gmail.com>
2025-02-25 21:34:46 +0800

    add 2502. Design Memory Allocator

---
+++ Code/Design Memory Allocator.cs
@@ -0,0 +1,165 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 2502. Design Memory Allocator
+    /// 
+    /// You are given an integer n representing the size of a 0-indexed memory array. All memory units are initially free.
+    /// 给定一个整数 n，表示 0 起始索引内存数组的大小。所有内存单元最初都是空闲的。
+    /// 
+    /// You have a memory allocator with the following functionalities:
+    /// 您有一个具有以下功能的内存分配器：
+    /// 
+    /// 1. Allocate a block of size consecutive free memory units and assign it the id mID.
+    /// 1. 分配一个大小为连续空闲内存单元的块，并为其分配 id mID。
+    /// 
+    /// 2. Free all memory units with the given id mID.
+    /// 2. 释放所有具有给定 id mID 的内存单元。
+    /// 
+    /// Note that:
+    /// 请注意：
+    /// 
+    /// * Multiple blocks can be allocated to the same mID.
+    /// * 可以将多个块分配给同一个 mID。
+    /// 
+    /// * You should free all the memory units with mID, even if they were allocated in different blocks.
+    /// * 您应该释放所有具有 mID 的内存单元，即使它们分配在不同的块中。
+    /// 
+    /// Implement the Allocator class:
+    /// 实现 Allocator 类：
+    /// 
+    /// * Allocator(int n) Initializes an Allocator object with a memory array of size n.
+    /// * Allocator(int n) 使用大小为 n 的内存数组初始化 Allocator 对象。
+    /// 
+    /// * int allocate(int size, int mID) Find the leftmost block of size consecutive free memory units and allocate it with the id mID. Return the block's first index. If such a block does not exist, return -1.
+    /// * int allocate(int size, int mID) 查找最左边的 size 个连续空闲内存单元块，并将其分配为 id mID。返回该块的第一个索引。如果不存在这样的块，则返回 -1。
+    /// 
+    /// * int freeMemory(int mID) Free all memory units with the id mID. Return the number of memory units you have freed.
+    /// * int freeMemory(int mID) 释放所有 id 为 mID 的内存单元。返回已释放的内存单元数。
+    /// 
+    /// </summary>
+    public class Design_Memory_Allocator
+    {
+        public class Allocator
+        {
+            byte[] blocks;
+            IDictionary<int, IList<int[]>> maps;
+
+            public Allocator(int n)
+            {
+                blocks = new byte[n];
+                maps = new Dictionary<int, IList<int[]>>();
+            }
+
+            public int Allocate(int size, int mID)
+            {
+                int startIndex = 0;
+                int count = 0;
+                for (var i = 0; i < blocks.Length; i++)
+                {
+                    if ((i == 0 || blocks[i - 1] == 1) && blocks[i] == 0)
+                    {
+                        startIndex = i;
+                        count = 1;
+                    }
+
+                    if (i > 0 && blocks[i - 1] == 0 && blocks[i] == 0)
+                    {
+                        count++;
+                    }
+
+                    if (count >= size)
+                    {
+                        break;
+                    }
+                }
+
+                if (count < size)
+                {
+                    return -1;
+                }
+
+                for (var i = startIndex; i < startIndex + size; i++)
+                {
+                    blocks[i] = 1;
+                }
+
+                if (!maps.ContainsKey(mID))
+                {
+                    maps[mID] = new List<int[]>();
+                }
+                maps[mID].Add([startIndex, size]);
+
+                return startIndex;
+            }
+
+            public int FreeMemory(int mID)
+            {
+                if (!maps.ContainsKey(mID)) return 0;
+
+                int ans = 0;
+                foreach (var map in maps[mID])
+                {
+                    ans += map[1];
+                    for (var i = map[0]; i < map[0] + map[1]; i++)
+                    {
+                        blocks[i] = 0;
+                    }
+                }
+                maps[mID] = new List<int[]>();
+                return ans;
+            }
+        }
+
+
+        public class Allocator2
+        {
+            private int n;
+            private int[] memory;
+
+            public Allocator2(int n)
+            {
+                this.n = n;
+                this.memory = new int[n];
+            }
+
+            public int Allocate(int size, int mID)
+            {
+                int count = 0;
+                for (int i = 0; i < n; ++i)
+                {
+                    if (memory[i] != 0)
+                    {
+                        count = 0;
+                    }
+                    else
+                    {
+                        ++count;
+                        if (count == size)
+                        {
+                            for (int j = i - count + 1; j <= i; ++j)
+                            {
+                                memory[j] = mID;
+                            }
+                            return i - count + 1;
+                        }
+                    }
+                }
+                return -1;
+            }
+
+            public int FreeMemory(int mID)
+            {
+                int count = 0;
+                for (int i = 0; i < n; ++i)
+                {
+                    if (memory[i] == mID)
+                    {
+                        ++count;
+                        memory[i] = 0;
+                    }
+                }
+                return count;
+            }
+        }
+    }
+}
---
+++ Test/Design Memory Allocator Test.cs
@@ -0,0 +1,63 @@
+﻿using static Code.Design_Memory_Allocator;
+
+namespace Test
+{
+    public class Design_Memory_Allocator_Test
+    {
+        public static TheoryData<int, string[], int[][], int[]> TestData => new TheoryData<int, string[], int[][], int[]>()
+        {
+            { 10,["allocate", "allocate", "allocate", "freeMemory", "allocate", "allocate", "allocate", "freeMemory", "allocate", "freeMemory"],[[1, 1], [1, 2], [1, 3], [2], [3, 4], [1, 1], [1, 1], [1], [10, 2], [7]],[0, 1, 2, 1, 3, 1, 6, 3, -1, 0]},
+            { 50,["allocate","allocate","allocate","allocate","freeMemory","freeMemory","freeMemory","allocate","allocate","allocate","allocate","freeMemory","freeMemory","freeMemory","freeMemory","freeMemory","freeMemory","freeMemory","allocate","freeMemory","freeMemory","allocate","freeMemory","allocate","allocate","freeMemory","freeMemory","freeMemory","allocate","allocate","allocate","allocate","freeMemory","allocate","freeMemory","freeMemory","allocate","allocate","allocate","allocate","allocate","allocate","allocate","freeMemory","freeMemory","freeMemory","freeMemory"],[[12,6],[28,16],[17,23],[50,23],[6],[10],[10],[16,8],[17,41],[44,27],[12,45],[33],[8],[16],[23],[23],[23],[29],[38,32],[29],[6],[40,11],[16],[22,33],[27,5],[3],[10],[29],[16,14],[46,47],[48,9],[36,17],[33],[14,24],[16],[8],[2,50],[31,36],[17,45],[46,31],[2,6],[16,2],[39,30],[33],[45],[30],[27]],[0,12,-1,-1,12,0,0,-1,-1,-1,0,0,0,28,0,0,0,0,12,0,0,-1,0,-1,-1,0,0,0,-1,-1,-1,-1,0,-1,0,0,-1,-1,-1,-1,-1,-1,-1,0,12,0,0]},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int n, string[] commands, int[][] paramters, int[] expected)
+        {
+            var actual = new int[commands.Length];
+            Allocator obj = new Allocator(n);
+
+            for (var i = 0; i < commands.Length; i++)
+            {
+                switch (commands[i])
+                {
+                    case "allocate":
+                        actual[i] = obj.Allocate(paramters[i][0], paramters[i][1]);
+                        break;
+                    case "freeMemory":
+                        actual[i] = obj.FreeMemory(paramters[i][0]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test2(int n, string[] commands, int[][] paramters, int[] expected)
+        {
+            var actual = new int[commands.Length];
+            Allocator2 obj = new Allocator2(n);
+
+            for (var i = 0; i < commands.Length; i++)
+            {
+                switch (commands[i])
+                {
+                    case "allocate":
+                        actual[i] = obj.Allocate(paramters[i][0], paramters[i][1]);
+                        break;
+                    case "freeMemory":
+                        actual[i] = obj.FreeMemory(paramters[i][0]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
