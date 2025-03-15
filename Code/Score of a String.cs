026d75cc9c1b6f402dd2e0ff732a1604509e7ebd
dandeilit <dande.i.lit@gmail.com>
2025-03-15 16:13:28 +0800

    add 3110. Score of a String

---
+++ Code/Score of a String.cs
@@ -0,0 +1,26 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 3110. Score of a String
+    /// 3110. 字符串的分数
+    /// 
+    /// You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+    /// 给你一个字符串 s 。一个字符串的 分数 定义为相邻字符 ASCII 码差值绝对值的和。
+    /// 
+    /// Return the score of s.
+    /// 请你返回 s 的 分数 。
+    /// 
+    /// </summary>
+    public class Score_of_a_String
+    {
+        public int ScoreOfString(string s)
+        {
+            int ans = 0;
+            for (int i = 1; i < s.Length; i++)
+            {
+                ans += Math.Abs(s[i] - s[i - 1]);
+            }
+            return ans;
+        }
+    }
+}
---
+++ Test/Score of a String Test.cs
@@ -0,0 +1,23 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Score_of_a_String_Test
+    {
+        public static TheoryData<string, int> TestData => new TheoryData<string, int>()
+        {
+            { "hello",13},
+            { "zaz",50},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(string s, int expected)
+        {
+            var score_of_a_String = new Score_of_a_String();
+            var actual = score_of_a_String.ScoreOfString(s);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
