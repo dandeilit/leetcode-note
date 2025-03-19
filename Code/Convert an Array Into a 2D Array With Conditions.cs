049ec5d7b6b0bc53026957fee03dc5b4a41d45fd
dandeilit <dande.i.lit@gmail.com>
2025-03-19 10:36:27 +0800

    add 2610. Convert an Array Into a 2D Array With Conditions

---
+++ Code/Convert an Array Into a 2D Array With Conditions.cs
@@ -0,0 +1,56 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 2610. Convert an Array Into a 2D Array With Conditions
+    /// 2610. 转换二维数组
+    /// 
+    /// You are given an integer array nums. You need to create a 2D array from nums satisfying the following conditions:
+    /// 给你一个整数数组 nums 。请你创建一个满足以下条件的二维数组：
+    /// 
+    /// * The 2D array should contain only the elements of the array nums.
+    /// * 二维数组应该 只 包含数组 nums 中的元素。
+    /// 
+    /// * Each row in the 2D array contains distinct integers.
+    /// * 二维数组中的每一行都包含 不同 的整数。
+    /// 
+    /// * The number of rows in the 2D array should be minimal.
+    /// * 二维数组的行数应尽可能 少 。
+    /// 
+    /// Return the resulting array. If there are multiple answers, return any of them.
+    /// 返回结果数组。如果存在多种答案，则返回其中任何一种。
+    /// 
+    /// Note that the 2D array can have a different number of elements on each row.
+    /// 请注意，二维数组的每一行上可以存在不同数量的元素。
+    /// 
+    /// </summary>
+    public class Convert_an_Array_Into_a_2D_Array_With_Conditions
+    {
+        public IList<IList<int>> FindMatrix(int[] nums)
+        {
+            var ans = new List<IList<int>>();
+            var dic = new Dictionary<int, int>();
+
+            foreach (var num in nums)
+            {
+                if (dic.ContainsKey(num))
+                {
+                    dic[num]++;
+                }
+                else
+                {
+                    dic.Add(num, 1);
+                }
+
+                if (ans.Count >= dic[num])
+                {
+                    ans[dic[num] - 1].Add(num);
+                }
+                else
+                {
+                    ans.Add(new List<int> { num });
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ Test/Convert an Array Into a 2D Array With Conditions Test.cs
@@ -0,0 +1,42 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Convert_an_Array_Into_a_2D_Array_With_Conditions_Test
+    {
+        public static TheoryData<int[], int[][]> TestData => new TheoryData<int[], int[][]>()
+        {
+            { [1,3,4,1,2,3,1],[[1,3,4,2],[1,3],[1]]},
+            { [1,2,3,4],[[4,3,2,1]]},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, int[][] expected)
+        {
+            var convert_an_Array_Into_a_2D_Array_With_Conditions = new Convert_an_Array_Into_a_2D_Array_With_Conditions();
+            var actual = convert_an_Array_Into_a_2D_Array_With_Conditions.FindMatrix(nums);
+
+            if (actual.Count() > expected.Length) Assert.Fail("row number is bigger");
+
+            for (var i = 0; i < actual.Count(); i++)
+            {
+                if (actual[i].Distinct().Count() != actual[i].Count)
+                {
+                    Assert.Fail("contain same integers");
+                    break;
+                }
+
+                for (var j = 0; j < actual[i].Count(); j++)
+                {
+                    if (!nums.Contains(actual[i][j]))
+                    {
+                        Assert.Fail("contain error elements");
+                        break;
+                    }
+                }
+            }
+            Assert.True(true);
+        }
+    }
+}
