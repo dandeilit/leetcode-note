66df7e0ac72f75d6c07ee91ad8a221d07baa4468
dandeilit <dande.i.lit@gmail.com>
2025-03-24 15:26:54 +0800

    add 2255. Count Prefixes of a Given String

---
+++ Code/Count Prefixes of a Given String.cs
@@ -0,0 +1,46 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 2255. Count Prefixes of a Given String
+    /// 2255. 统计是给定字符串前缀的字符串数目
+    /// 
+    /// You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+    /// 给你一个字符串数组 words 和一个字符串 s ，其中 words[i] 和 s 只包含 小写英文字母 。
+    /// 
+    /// Return the number of strings in words that are a prefix of s.
+    /// 请你返回 words 中是字符串 s 前缀 的 字符串数目 。
+    /// 
+    /// A prefix of a string is a substring that occurs at the beginning of the string. A substring is a contiguous sequence of characters within a string.
+    /// 一个字符串的 前缀 是出现在字符串开头的子字符串。子字符串 是一个字符串中的连续一段字符序列。
+    /// 
+    /// </summary>
+    public class Count_Prefixes_of_a_Given_String
+    {
+        public int CountPrefixes(string[] words, string s)
+        {
+            int num = 0;
+            foreach (var word in words)
+            {
+                if (word.Length <= s.Length)
+                    num += word.Equals(s.Substring(0, word.Length)) ? 1 : 0;
+            }
+            return num;
+        }
+
+        public int CountPrefixes2(string[] words, string s)
+        {
+            HashSet<string> prefixes = new HashSet<string>();
+            for (var i = 1; i <= s.Length; i++)
+            {
+                prefixes.Add(s.Substring(0, i));
+            }
+
+            int num = 0;
+            foreach (var word in words)
+            {
+                if (prefixes.Contains(word)) num++;
+            }
+            return num;
+        }
+    }
+}
---
+++ Test/Count Prefixes of a Given String Test.cs
@@ -0,0 +1,31 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Count_Prefixes_of_a_Given_String_Test
+    {
+        public static TheoryData<string[], string, int> TestData => new TheoryData<string[], string, int>()
+        {
+            { ["a","b","c","ab","bc","abc"],"abc",3},
+            { ["a","a"],"aa",2},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(string[] words, string s, int expected)
+        {
+            var count_Prefixes_of_a_Given_String = new Count_Prefixes_of_a_Given_String();
+            var actual = count_Prefixes_of_a_Given_String.CountPrefixes(words, s);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test2(string[] words, string s, int expected)
+        {
+            var count_Prefixes_of_a_Given_String = new Count_Prefixes_of_a_Given_String();
+            var actual = count_Prefixes_of_a_Given_String.CountPrefixes2(words, s);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
