264b3675a3df0d3ee88fb031a144cf8efe731fe2
dandeilit <dande.i.lit@gmail.com>
2025-03-31 12:19:49 +0800

    add 2278. Percentage of Letter in String

---
+++ Code/Percentage of Letter in String.cs
@@ -0,0 +1,23 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 2278. Percentage of Letter in String
+    /// 2278. 字母在字符串中的百分比
+    /// 
+    /// Given a string s and a character letter, return the percentage of characters in s that equal letter rounded down to the nearest whole percent.
+    /// 给你一个字符串 s 和一个字符 letter ，返回在 s 中等于 letter 字符所占的 百分比 ，向下取整到最接近的百分比。
+    /// 
+    /// </summary>
+    public class Percentage_of_Letter_in_String
+    {
+        public int PercentageLetter(string s, char letter)
+        {
+            int num = 0;
+            foreach (var c in s)
+            {
+                if (c == letter) num++;
+            }
+            return num * 100 / s.Length;
+        }
+    }
+}
---
+++ Test/Percentage of Letter in String Test.cs
@@ -0,0 +1,22 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Percentage_of_Letter_in_String_Test
+    {
+        public static TheoryData<string, char, int> TestData => new TheoryData<string, char, int>()
+        {
+            { "foobar",'o',33},
+            { "jjjj",'k',0},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(string s, char letter, int expected)
+        {
+            var percentage_of_Letter_in_String = new Percentage_of_Letter_in_String();
+            var actual = percentage_of_Letter_in_String.PercentageLetter(s, letter);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
