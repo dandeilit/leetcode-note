92475099427a290376f0c54fb9b6141a6107e7e9
dandeilit <dande.i.lit@gmail.com>
2025-04-02 15:37:36 +0800

    add 2873. Maximum Value of an Ordered Triplet I

---
+++ Code/Maximum Value of an Ordered Triplet I.cs
@@ -0,0 +1,110 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 2873. Maximum Value of an Ordered Triplet I
+    /// 2873. 有序三元组中的最大值 I
+    /// 
+    /// You are given a 0-indexed integer array nums.
+    /// 给你一个下标从 0 开始的整数数组 nums 。
+    /// 
+    /// Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+    /// 请你从所有满足 i < j < k 的下标三元组 (i, j, k) 中，找出并返回下标三元组的最大值。如果所有满足条件的三元组的值都是负数，则返回 0 。
+    /// 
+    /// The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+    /// 下标三元组 (i, j, k) 的值等于 (nums[i] - nums[j]) * nums[k] 。
+    /// 
+    /// </summary>
+    public class Maximum_Value_of_an_Ordered_Triplet_I
+    {
+        /// <summary>
+        /// 暴力枚举
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public long MaximumTripletValue(int[] nums)
+        {
+            int n = nums.Length;
+            long ans = 0;
+            for (var i = 0; i < n - 2; i++)
+            {
+                for (int j = i + 1; j < n - 1; j++)
+                {
+                    long diff = nums[i] - nums[j];
+                    if (diff <= 0) continue;
+
+                    for (int k = j + 1; k < n; k++)
+                    {
+                        ans = Math.Max(diff * nums[k], ans);
+                    }
+                }
+            }
+            return ans;
+        }
+
+        /// <summary>
+        /// 贪心
+        /// 利用 m 维护 [0,j) 的最大值
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public long MaximumTripletValue2(int[] nums)
+        {
+            int n = nums.Length;
+            long res = 0;
+            for (int k = 2; k < n; k++)
+            {
+                int m = nums[0];
+                for (int j = 1; j < k; j++)
+                {
+                    res = Math.Max(res, (long)(m - nums[j]) * nums[k]);
+                    m = Math.Max(m, nums[j]);
+                }
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// 贪心 + 前后缀数组
+        /// 当固定 j 时，nums[i] 和 nums[k] 分别取 [0,j) 和 [j+1,n) 的最大值时，三元组的值最大。
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public long MaximumTripletValue3(int[] nums)
+        {
+            int n = nums.Length;
+            int[] leftMax = new int[n];
+            int[] rightMax = new int[n];
+            for (int i = 1; i < n; i++)
+            {
+                leftMax[i] = Math.Max(leftMax[i - 1], nums[i - 1]);
+                rightMax[n - 1 - i] = Math.Max(rightMax[n - i], nums[n - i]);
+            }
+            long res = 0;
+            for (int j = 1; j < n - 1; j++)
+            {
+                res = Math.Max(res, (long)(leftMax[j] - nums[j]) * rightMax[j]);
+            }
+            return res;
+        }
+
+
+        /// <summary>
+        /// 贪心
+        /// 用 imax 维护 nums[i] 的最大值，dmax 维护 nums[i]−nums[j] 的最大值
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public long MaximumTripletValue4(int[] nums)
+        {
+            int n = nums.Length;
+            long res = 0, imax = 0, dmax = 0;
+            for (int k = 0; k < n; k++)
+            {
+                res = Math.Max(res, dmax * nums[k]);
+                dmax = Math.Max(dmax, imax - nums[k]);
+                imax = Math.Max(imax, nums[k]);
+            }
+            return res;
+        }
+    }
+}
---
+++ Test/Maximum Value of an Ordered Triplet I Test.cs
@@ -0,0 +1,50 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Maximum_Value_of_an_Ordered_Triplet_I_Test
+    {
+        public static TheoryData<int[], long> TestData => new TheoryData<int[], long>()
+        {
+            { [12,6,1,2,7],77},
+            { [1,10,3,4,19],133},
+            { [1,2,3],0},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, long expected)
+        {
+            var maximum_Value_of_an_Ordered_Triplet_I = new Maximum_Value_of_an_Ordered_Triplet_I();
+            var actual = maximum_Value_of_an_Ordered_Triplet_I.MaximumTripletValue(nums);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test2(int[] nums, long expected)
+        {
+            var maximum_Value_of_an_Ordered_Triplet_I = new Maximum_Value_of_an_Ordered_Triplet_I();
+            var actual = maximum_Value_of_an_Ordered_Triplet_I.MaximumTripletValue(nums);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test3(int[] nums, long expected)
+        {
+            var maximum_Value_of_an_Ordered_Triplet_I = new Maximum_Value_of_an_Ordered_Triplet_I();
+            var actual = maximum_Value_of_an_Ordered_Triplet_I.MaximumTripletValue(nums);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test4(int[] nums, long expected)
+        {
+            var maximum_Value_of_an_Ordered_Triplet_I = new Maximum_Value_of_an_Ordered_Triplet_I();
+            var actual = maximum_Value_of_an_Ordered_Triplet_I.MaximumTripletValue(nums);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
