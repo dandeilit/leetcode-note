18336fda81dbfce00dcb06c2bb23fb55aca1c884
dandeilit <dande.i.lit@gmail.com>
2025-04-05 14:44:12 +0800

    add 1863. Sum of All Subset XOR Totals

---
+++ Code/Sum of All Subset XOR Totals.cs
@@ -0,0 +1,67 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 1863. Sum of All Subset XOR Totals
+    /// 1863. 找出所有子集的异或总和再求和
+    /// 
+    /// The XOR total of an array is defined as the bitwise XOR of all its elements, or 0 if the array is empty.
+    /// 一个数组的 异或总和 定义为数组中所有元素按位 XOR 的结果；如果数组为 空 ，则异或总和为 0 。
+    /// 
+    /// For example, the XOR total of the array [2,5,6] is 2 XOR 5 XOR 6 = 1.
+    /// 例如，数组 [2,5,6] 的 异或总和 为 2 XOR 5 XOR 6 = 1 。
+    /// 
+    /// Given an array nums, return the sum of all XOR totals for every subset of nums. 
+    /// 给你一个数组 nums ，请你求出 nums 中每个 子集 的 异或总和 ，计算并返回这些值相加之 和 。
+    /// 
+    /// Note: Subsets with the same elements should be counted multiple times.
+    /// 注意：在本题中，元素 相同 的不同子集应 多次 计数。
+    /// 
+    /// An array a is a subset of an array b if a can be obtained from b by deleting some (possibly zero) elements of b.
+    /// 数组 a 是数组 b 的一个 子集 的前提条件是：从 b 删除几个（也可能不删除）元素能够得到 a 。
+    /// 
+    /// </summary>
+    public class Sum_of_All_Subset_XOR_Totals
+    {
+        int res;
+        int n;
+
+        // 深度优先搜索
+        void Dfs(int val, int idx, int[] nums)
+        {
+            if (idx == n)
+            {
+                // 终止递归
+                res += val;
+                return;
+            }
+            // 考虑选择当前数字
+            Dfs(val ^ nums[idx], idx + 1, nums);
+            // 考虑不选择当前数字
+            Dfs(val, idx + 1, nums);
+        }
+
+        public int SubsetXORSum(int[] nums)
+        {
+            res = 0;
+            n = nums.Length;
+            Dfs(0, 0, nums);
+            return res;
+        }
+
+        /// <summary>
+        /// 按位考虑 + 二项式展开
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int SubsetXORSum2(int[] nums)
+        {
+            int res = 0;
+            int n = nums.Length;
+            foreach (int num in nums)
+            {
+                res |= num;
+            }
+            return res << (n - 1);
+        }
+    }
+}
---
+++ Test/Sum of All Subset XOR Totals Test.cs
@@ -0,0 +1,32 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Sum_of_All_Subset_XOR_Totals_Test
+    {
+        public static TheoryData<int[], int> TestData => new TheoryData<int[], int>()
+        {
+            { [1,3],6},
+            { [5,1,6],28},
+            { [3,4,5,6,7,8],480},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, int expected)
+        {
+            var sum_of_All_Subset_XOR_Totals = new Sum_of_All_Subset_XOR_Totals();
+            var actual = sum_of_All_Subset_XOR_Totals.SubsetXORSum(nums);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test2(int[] nums, int expected)
+        {
+            var sum_of_All_Subset_XOR_Totals = new Sum_of_All_Subset_XOR_Totals();
+            var actual = sum_of_All_Subset_XOR_Totals.SubsetXORSum2(nums);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
