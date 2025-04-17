45f56b2e138b2f5f66644b016d1a0a20e2091917
dandeilit <dande.i.lit@gmail.com>
2025-04-17 09:35:19 +0800

    add 2176. Count Equal and Divisible Pairs in an Array

---
+++ Code/Count Equal and Divisible Pairs in an Array.cs
@@ -0,0 +1,36 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 2176. Count Equal and Divisible Pairs in an Array
+    /// 2176. 统计数组中相等且可以被整除的数对
+    /// 
+    /// Given a 0-indexed integer array nums of length n and an integer k, return the number of pairs (i, j) where 0 <= i < j < n, such that nums[i] == nums[j] and (i * j) is divisible by k.
+    /// 给你一个下标从 0 开始长度为 n 的整数数组 nums 和一个整数 k ，请你返回满足 0 <= i < j < n ，nums[i] == nums[j] 且 (i * j) 能被 k 整除的数对 (i, j) 的 数目 。
+    /// 
+    /// </summary>
+    public class Count_Equal_and_Divisible_Pairs_in_an_Array
+    {
+        /// <summary>
+        /// 遍历数对
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int CountPairs(int[] nums, int k)
+        {
+            int ans = 0;
+            int n = nums.Length;
+            for (var i = 0; i < n - 1; i++)
+            {
+                for (var j = i + 1; j < n; j++)
+                {
+                    if (nums[i] == nums[j] && i * j % k == 0)
+                    {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ Test/Count Equal and Divisible Pairs in an Array Test.cs
@@ -0,0 +1,22 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Count_Equal_and_Divisible_Pairs_in_an_Array_Test
+    {
+        public static TheoryData<int[], int, int> TestData => new TheoryData<int[], int, int> {
+            { [3,1,2,2,2,1,3],2,4},
+            { [1,2,3,4],1,0},
+            { [5,5,9,2,5,5,9,2,2,5,5,6,2,2,5,2,5,4,3],7,18}
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, int k, int expected)
+        {
+            var count_Equal_and_Divisible_Pairs_in_an_Array = new Count_Equal_and_Divisible_Pairs_in_an_Array();
+            var actual = count_Equal_and_Divisible_Pairs_in_an_Array.CountPairs(nums, k);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
