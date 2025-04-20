73b039b153a6819198a7b47d7ebdb964f5c4281f
dandeilit <dande.i.lit@gmail.com>
2025-04-20 10:21:55 +0800

    add 781. Rabbits in Forest

---
+++ Code/Rabbits in Forest.cs
@@ -0,0 +1,39 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 781. Rabbits in Forest
+    /// 781. 森林中的兔子
+    /// 
+    /// There is a forest with an unknown number of rabbits. We asked n rabbits "How many rabbits have the same color as you?" and collected the answers in an integer array answers where answers[i] is the answer of the i-th rabbit.
+    /// 森林中有未知数量的兔子。提问其中若干只兔子 "还有多少只兔子与你（指被提问的兔子）颜色相同?" ，将答案收集到一个整数数组 answers 中，其中 answers[i] 是第 i 只兔子的回答。
+    /// 
+    /// Given the array answers, return the minimum number of rabbits that could be in the forest.
+    /// 给你数组 answers ，返回森林中兔子的最少数量。
+    /// 
+    /// </summary>
+    public class Rabbits_in_Forest
+    {
+        public int NumRabbits(int[] answers)
+        {
+            int ans = 0;
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (var i = 0; i < answers.Length; i++)
+            {
+                if (!map.ContainsKey(answers[i]))
+                {
+                    map.Add(answers[i], 1);
+                }
+                else
+                {
+                    map[answers[i]]++;
+                }
+            }
+
+            foreach (var item in map)
+            {
+                ans += (item.Key + item.Value) / (item.Key + 1) * (item.Key + 1);
+            }
+            return ans;
+        }
+    }
+}
---
+++ Test/Rabbits in Forest Test.cs
@@ -0,0 +1,23 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Rabbits_in_Forest_Test
+    {
+        public static TheoryData<int[], int> TestData => new TheoryData<int[], int> {
+            { [1,1,2],5},
+            { [10,10,10],11},
+            { [1,0,1,0,0],5},
+            { [0,0,1,1,1],6},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] answers, int expected)
+        {
+            var rabbits_in_Forest = new Rabbits_in_Forest();
+            var actual = rabbits_in_Forest.NumRabbits(answers);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
