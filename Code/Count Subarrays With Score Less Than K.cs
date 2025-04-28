09509fa5093c4c0b0198a065e50fda150b7ee6f9
dandeilit <dande.i.lit@gmail.com>
2025-04-28 20:53:31 +0800

    add 2302. Count Subarrays With Score Less Than K

---
+++ Code/Count Subarrays With Score Less Than K.cs
@@ -0,0 +1,46 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 2302. Count Subarrays With Score Less Than K
+    /// 2302. 统计得分小于 K 的子数组数目
+    /// 
+    /// The score of an array is defined as the product of its sum and its length.
+    /// 一个数组的 分数 定义为数组之和 乘以 数组的长度。
+    /// 
+    /// * For example, the score of [1, 2, 3, 4, 5] is (1 + 2 + 3 + 4 + 5) * 5 = 75.
+    /// * 比方说，[1, 2, 3, 4, 5] 的分数为 (1 + 2 + 3 + 4 + 5) * 5 = 75 。
+    /// 
+    /// Given a positive integer array nums and an integer k, return the number of non-empty subarrays of nums whose score is strictly less than k.
+    /// 给你一个正整数数组 nums 和一个整数 k ，请你返回 nums 中分数 严格小于 k 的 非空整数子数组数目。
+    /// 
+    /// A subarray is a contiguous sequence of elements within an array.
+    /// 子数组 是数组中的一个连续元素序列。
+    /// 
+    /// </summary>
+    public class Count_Subarrays_With_Score_Less_Than_K
+    {
+        /// <summary>
+        /// 滑动窗口
+        /// i 为子数组左端点，j 为子数组右端点
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public long CountSubarrays(int[] nums, long k)
+        {
+            int n = nums.Length;
+            long res = 0, total = 0;
+            for (int i = 0, j = 0; j < n; j++)
+            {
+                total += nums[j];
+                while (i <= j && total * (j - i + 1) >= k)
+                {
+                    total -= nums[i];
+                    i++;
+                }
+                res += j - i + 1;
+            }
+            return res;
+        }
+    }
+}
---
+++ Test/Count Subarrays With Score Less Than K Test.cs
@@ -0,0 +1,21 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Count_Subarrays_With_Score_Less_Than_K_Test
+    {
+        public static TheoryData<int[], long, long> TestData => new TheoryData<int[], long, long> {
+            { [2,1,4,3,5],10,6},
+            { [1,1,1],5,5},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, long k, long expected)
+        {
+            var count_Subarrays_With_Score_Less_Than_K = new Count_Subarrays_With_Score_Less_Than_K();
+            var actual = count_Subarrays_With_Score_Less_Than_K.CountSubarrays(nums, k);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
