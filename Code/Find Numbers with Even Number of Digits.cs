eb1a3bdd8257f5cfbd128c6dffc445738c6b7daf
dandeilit <dande.i.lit@gmail.com>
2025-04-30 17:36:18 +0800

    add 1295. Find Numbers with Even Number of Digits

---
+++ Code/Find Numbers with Even Number of Digits.cs
@@ -0,0 +1,37 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 1295. Find Numbers with Even Number of Digits
+    /// 1295. 统计位数为偶数的数字
+    /// 
+    /// Given an array nums of integers, return how many of them contain an even number of digits.
+    /// 给你一个整数数组 nums，请你返回其中包含 偶数 个数位的数字的个数。
+    /// 
+    /// </summary>
+    public class Find_Numbers_with_Even_Number_of_Digits
+    {
+        public int FindNumbers(int[] nums)
+        {
+            int ans = 0;
+            foreach (var num in nums)
+            {
+                if (GetLen(num) % 2 == 0)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+
+        private int GetLen(int num)
+        {
+            int len = 1;
+            while (num >= 10)
+            {
+                len++;
+                num = num / 10;
+            }
+            return len;
+        }
+    }
+}
---
+++ Test/Find Numbers with Even Number of Digits Test.cs
@@ -0,0 +1,22 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Find_Numbers_with_Even_Number_of_Digits_Test
+    {
+        public static TheoryData<int[], int> TestData => new TheoryData<int[], int> {
+            { [12,345,2,6,7896],2},
+            { [555,901,482,1771],1},
+            { [100000],1},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] nums, int expected)
+        {
+            var find_Numbers_with_Even_Number_of_Digits = new Find_Numbers_with_Even_Number_of_Digits();
+            var actual = find_Numbers_with_Even_Number_of_Digits.FindNumbers(nums);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
