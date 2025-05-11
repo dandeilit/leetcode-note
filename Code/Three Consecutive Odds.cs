905a4686961a2504d9783d4b1429d39cef79dacd
dandeilit <dande.i.lit@gmail.com>
2025-05-11 21:02:55 +0800

    add 1550. Three Consecutive Odds

---
+++ Code/Three Consecutive Odds.cs
@@ -0,0 +1,34 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 1550. Three Consecutive Odds
+    /// 1550. 存在连续三个奇数的数组
+    /// 
+    /// Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+    /// 给你一个整数数组 arr，请你判断数组中是否存在连续三个元素都是奇数的情况：如果存在，请返回 true ；否则，返回 false 。
+    /// 
+    /// </summary>
+    public class Three_Consecutive_Odds
+    {
+        public bool ThreeConsecutiveOdds(int[] arr)
+        {
+            int num = 0;
+            foreach (var item in arr)
+            {
+                if (item % 2 == 0)
+                {
+                    num = 0;
+                }
+                else
+                {
+                    num++;
+                    if (num >= 3)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Test/Three Consecutive Odds Test.cs
@@ -0,0 +1,21 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Three_Consecutive_Odds_Test
+    {
+        public static TheoryData<int[], bool> TestData => new TheoryData<int[], bool> {
+            { [2,6,4,1],false},
+            { [1,2,34,3,4,5,7,23,12],true},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int[] arr, bool expected)
+        {
+            var three_Consecutive_Odds = new Three_Consecutive_Odds();
+            var actual = three_Consecutive_Odds.ThreeConsecutiveOdds(arr);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
