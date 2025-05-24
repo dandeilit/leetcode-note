1c7fa7bf92e64ac5c7ae09467d84dba022b214b7
dandeilit <dande.i.lit@gmail.com>
2025-05-24 16:41:22 +0800

    add 2942. Find Words Containing Character

---
+++ Code/Find Words Containing Character.cs
@@ -0,0 +1,42 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 2942. Find Words Containing Character
+    /// 2942. 查找包含给定字符的单词
+    /// 
+    /// You are given a 0-indexed array of strings words and a character x.
+    /// 给你一个下标从 0 开始的字符串数组 words 和一个字符 x 。
+    /// 
+    /// Return an array of indices representing the words that contain the character x.
+    /// 请你返回一个 下标数组 ，表示下标在数组中对应的单词包含字符 x 。
+    /// 
+    /// Note that the returned array may be in any order.
+    /// 注意 ，返回的数组可以是 任意 顺序。
+    /// 
+    /// </summary>
+    public class Find_Words_Containing_Character
+    {
+        /// <summary>
+        /// 模拟
+        /// </summary>
+        /// <param name="words"></param>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public IList<int> FindWordsContaining(string[] words, char x)
+        {
+            var ans = new List<int>();
+            for (var i = 0; i < words.Length; i++)
+            {
+                foreach (var c in words[i])
+                {
+                    if (c == x)
+                    {
+                        ans.Add(i);
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ Test/Find Words Containing Character Test.cs
@@ -0,0 +1,22 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Find_Words_Containing_Character_Test
+    {
+        public static TheoryData<string[], char, int[]> TestData => new TheoryData<string[], char, int[]> {
+            { ["leet","code"],'e',[0,1]},
+            { ["abc","bcd","aaaa","cbc"],'a',[0,2]},
+            { ["abc","bcd","aaaa","cbc"],'z',[]},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(string[] words, char x, int[] expected)
+        {
+            var find_Words_Containing_Character = new Find_Words_Containing_Character();
+            var actual = find_Words_Containing_Character.FindWordsContaining(words, x);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
