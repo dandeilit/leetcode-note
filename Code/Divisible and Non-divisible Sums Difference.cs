ecbe2bef29f65e4e05fb09f038bcc4ff24ede961
dandeilit <dande.i.lit@gmail.com>
2025-05-27 20:31:19 +0800

    add 2894. Divisible and Non-divisible Sums Difference

---
+++ Code/Divisible and Non-divisible Sums Difference.cs
@@ -0,0 +1,54 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 2894. Divisible and Non-divisible Sums Difference
+    /// 2894. 分类求和并作差
+    /// 
+    /// You are given positive integers n and m.
+    /// 给你两个正整数 n 和 m 。
+    /// 
+    /// Define two integers as follows:
+    /// 现定义两个整数 num1 和 num2 ，如下所示：
+    /// 
+    /// * num1: The sum of all integers in the range [1, n] (both inclusive) that are not divisible by m.
+    /// * num1：范围 [1, n] 内所有 无法被 m 整除 的整数之和。
+    /// 
+    /// * num2: The sum of all integers in the range [1, n] (both inclusive) that are divisible by m.
+    /// * num2：范围 [1, n] 内所有 能够被 m 整除 的整数之和。
+    /// 
+    /// Return the integer num1 - num2.
+    /// 返回整数 num1 - num2 。
+    /// 
+    /// </summary>
+    public class Divisible_and_Non_divisible_Sums_Difference
+    {
+        /// <summary>
+        /// 遍历
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public int DifferenceOfSums(int n, int m)
+        {
+            int num2 = 0;
+            int num = m;
+            while (num <= n)
+            {
+                num2 += num;
+                num += m;
+            }
+            return (1 + n) * n / 2 - num2 - num2;
+        }
+
+        /// <summary>
+        /// 数学推导
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public int DifferenceOfSums2(int n, int m)
+        {
+            return (1 + n) * n / 2 - (m + n - n % m) * (n / m);
+        }
+    }
+}
---
+++ Test/Divisible and Non-divisible Sums Difference Test.cs
@@ -0,0 +1,31 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Divisible_and_Non_divisible_Sums_Difference_Test
+    {
+        public static TheoryData<int, int, int> TestData => new TheoryData<int, int, int> {
+            { 10,3,19},
+            { 5,6,15},
+            { 5,1,-15},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int n, int m, int expected)
+        {
+            var divisible_and_Non_divisible_Sums_Difference = new Divisible_and_Non_divisible_Sums_Difference();
+            var actual = divisible_and_Non_divisible_Sums_Difference.DifferenceOfSums(n, m);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test2(int n, int m, int expected)
+        {
+            var divisible_and_Non_divisible_Sums_Difference = new Divisible_and_Non_divisible_Sums_Difference();
+            var actual = divisible_and_Non_divisible_Sums_Difference.DifferenceOfSums2(n, m);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
