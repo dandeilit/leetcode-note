1d05eb18c6fffbf33a32d660e9e479e4b733bad4
dandeilit <dande.i.lit@gmail.com>
2025-06-10 20:09:09 +0800

    add 386. Lexicographical Numbers

---
+++ Code/Lexicographical Numbers.cs
@@ -0,0 +1,47 @@
+﻿namespace Code
+{
+    /// <summary>
+    /// 386. Lexicographical Numbers
+    /// 386. 字典序排数
+    /// 
+    /// Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+    /// 给你一个整数 n ，按字典序返回范围 [1, n] 内所有整数。
+    /// 
+    /// You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
+    /// 你必须设计一个时间复杂度为 O(n) 且使用 O(1) 额外空间的算法。
+    /// 
+    /// </summary>
+    public class Lexicographical_Numbers
+    {
+        /// <summary>
+        /// 深度优先搜索
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public IList<int> LexicalOrder(int n)
+        {
+            IList<int> ret = new List<int>();
+            int number = 1;
+            for (int i = 0; i < n; i++)
+            {
+                ret.Add(number);
+
+                // 尝试在 number 后面附加一个零，即 number * 10，如果 number * 10 <= n，那么说明 number * 10 是下一个字典序整数；
+                if (number * 10 <= n)
+                {
+                    number *= 10;
+                }
+                else
+                {
+                    // 如果 number mod 10 = 9 或 number + 1 > n，那么说明末尾的数位已经搜索完成，退回上一位，即 number = number / 10，然后继续判断直到 number mod 10 != 9 且 number + 1 <= n 为止，那么 number + 1 是下一个字典序整数。
+                    while (number % 10 == 9 || number + 1 > n)
+                    {
+                        number /= 10;
+                    }
+                    number++;
+                }
+            }
+            return ret;
+        }
+    }
+}
---
+++ Test/Lexicographical Numbers Test.cs
@@ -0,0 +1,21 @@
+﻿using Code;
+
+namespace Test
+{
+    public class Lexicographical_Numbers_Test
+    {
+        public static TheoryData<int, int[]> TestData => new TheoryData<int, int[]> {
+            { 13,[1,10,11,12,13,2,3,4,5,6,7,8,9]},
+            { 2,[1,2]},
+        };
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Test(int n, int[] expected)
+        {
+            var lexicographical_Numbers = new Lexicographical_Numbers();
+            var actual = lexicographical_Numbers.LexicalOrder(n);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
